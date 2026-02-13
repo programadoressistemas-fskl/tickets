b98fdfb406fd4fda013754272e81453247950c1e
Karen Navarro Hurtado <karen.navarro.hurtado@gmail.com>
2026-02-13 15:09:54 -0600

    Actualizacion de creacion de login y home

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './admin/home/home';
+import { Login } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: '',
+    component: Home
+  }
+];
