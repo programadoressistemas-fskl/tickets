85ed6deaac2fb5edc9c55e75a296582fd038e34e
Karen Navarro Hurtado <karen.navarro.hurtado@gmail.com>
2026-02-17 12:24:07 -0600

    empece a realizar el formulario de registro

--- front-end/src/app/auth/login/login.ts
+++ front-end/src/app/auth/login/login.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ɵInternalFormsSharedModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
-  // Aquí va la lógica de autenticación después
+export class Login implements OnInit {
+  private formLogin!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.crearFormLogin();
+  }
+
+  private crearFormLogin(): void {
+    this.formLogin = this.fb.group({
+      correo: ['prueba', [Validators.required, Validators.email, Validators.pattern('[a-zA-Zá-úÁ-Ú0-9 .,-_:@#$%&+{}()?¿!¡\n]*')]],
+      password: [null, [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-Ú0-9 .,-_:@#$%&+{}()?¿!¡\n]*')]]
+    });
+  }
 }
