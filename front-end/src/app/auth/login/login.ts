52a01f22658d9d97a54a1025780f0bc758378ffc
Adrián Villa <idgs.adrian.villa@gmail.com>
2026-02-17 16:28:04 -0600

    Se agregan cambios de formulario reactivo

--- front-end/src/app/auth/login/login.ts
+++ front-end/src/app/auth/login/login.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ɵInternalFormsSharedModule],
+  imports: [CommonModule, ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login implements OnInit {
-  private formLogin!: FormGroup;
+  protected formLogin!: FormGroup;
 
   constructor(
     private fb: FormBuilder
@@ -22,7 +23,7 @@ export class Login implements OnInit {
 
   private crearFormLogin(): void {
     this.formLogin = this.fb.group({
-      correo: ['prueba', [Validators.required, Validators.email, Validators.pattern('[a-zA-Zá-úÁ-Ú0-9 .,-_:@#$%&+{}()?¿!¡\n]*')]],
+      correo: [null, [Validators.required, Validators.email, Validators.pattern('[a-zA-Zá-úÁ-Ú0-9 .,-_:@#$%&+{}()?¿!¡\n]*')]],
       password: [null, [Validators.required, Validators.pattern('[a-zA-Zá-úÁ-Ú0-9 .,-_:@#$%&+{}()?¿!¡\n]*')]]
     });
   }
