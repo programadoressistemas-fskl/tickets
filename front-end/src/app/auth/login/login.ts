3ff7f51e38d0cecb65dccd7db308064b1d3280b2
Karen Navarro Hurtado <karen.navarro.hurtado@gmail.com>
2026-02-17 10:52:22 -0600

    Realice el LOGIN e incorpore la seccion imagenes y diseño

--- front-end/src/app/auth/login/login.ts
+++ front-end/src/app/auth/login/login.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-
+  // Aquí va la lógica de autenticación después
 }
