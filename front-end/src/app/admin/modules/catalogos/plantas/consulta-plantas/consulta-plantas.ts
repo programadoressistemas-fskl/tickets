e767385ff3e6a6b8e4a13665f2430e90ed1d6aee
neitan-02 <hernandezmorenonathan09@gmail.com>
2026-02-27 12:33:29 -0600

    mejora estructural de codigo visual

--- front-end/src/app/admin/modules/catalogos/plantas/consulta-plantas/consulta-plantas.ts
+++ front-end/src/app/admin/modules/catalogos/plantas/consulta-plantas/consulta-plantas.ts
@@ -3,29 +3,29 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { PlantasService } from '../../../../services/api/plantas/plantas';
 
 @Component({
-  selector: 'app-consulta-plantas',
-  imports: [CommonModule],
-  standalone: true,
-  templateUrl: './consulta-plantas.html',
-  styleUrl: './consulta-plantas.css',
+	selector: 'app-consulta-plantas',
+	imports: [CommonModule],
+	standalone: true,
+	templateUrl: './consulta-plantas.html',
+	styleUrl: './consulta-plantas.css',
 })
 export class ConsultaPlantas implements OnInit {
-  protected datosTabla: any = [];
-  constructor(
-    private plantas: PlantasService,
-    private ch: ChangeDetectorRef 
-  ) { }
+	protected datosTabla: any = [];
+	constructor(
+		private plantas: PlantasService,
+		private ch: ChangeDetectorRef
+	) { }
 
-  ngOnInit(): void {
-    this.obtnerListaPlantas();
-  }
+	ngOnInit(): void {
+		this.obtnerListaPlantas();
+	}
 
-  public async obtnerListaPlantas(): Promise<any>{
-    return this.plantas.obtenerListaPlantas().toPromise().then(
-      respuesta=> {
-        this.datosTabla = respuesta.plantas;
-        this.ch.markForCheck(); 
-      }
-    )
-  }
+	public async obtnerListaPlantas(): Promise<any> {
+		return this.plantas.obtenerListaPlantas().toPromise().then(
+			respuesta => {
+				this.datosTabla = respuesta.plantas;
+				this.ch.markForCheck();
+			}
+		)
+	}
 }
