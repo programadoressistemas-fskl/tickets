609dd878c87e7458a09c8f341e22ac10565b13ce
neitan-02 <hernandezmorenonathan09@gmail.com>
2026-03-02 09:54:12 -0600

    Se crea la maqueta de registro usuarios

--- front-end/src/app/admin/modules/usuarios/registrar-usuario/registrar-usuario.ts
+++ front-end/src/app/admin/modules/usuarios/registrar-usuario/registrar-usuario.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { ModalService } from '../../../services/modal/modal';
+import { CommonModule } from '@angular/common';
+import { AreasService } from '../../../services/api/areas/areas';
 
 @Component({
 	selector: 'app-registrar-usuario',
-	imports: [],
+	imports: [CommonModule],
+	standalone: true,
 	templateUrl: './registrar-usuario.html',
 	styleUrl: './registrar-usuario.css',
 })
 export class RegistrarUsuario {
+	protected listaAreas: any[] = [];
+
 	constructor(
-		private modal: ModalService
+		private modal: ModalService,
+		private areas: AreasService,
+		private ch: ChangeDetectorRef
 	) { }
+
+	ngOnInit(): void {
+		this.obtenerListaAreas();
+	}
+
+	public async obtenerListaAreas(): Promise<any> {
+		return this.areas.obtenerListaAreas().toPromise().then(
+			respuesta => {
+				this.listaAreas = respuesta.areas;
+				this.ch.markForCheck();
+			} 
+		)
+	}
 
 	public cerrarModal(): void {
 		this.modal.cerrarModal();
-	}
+	} 
+
+
 }
